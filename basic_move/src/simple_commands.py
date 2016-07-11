6cf786d41b80b7bdf58d7ebdfdcd68cd21cd1247
anishau <anisha@uppugonduri.net>
2016-07-10 22:50:53 -0700

    Update simple_commands.py

--- basic_move/src/simple_commands.py
+++ basic_move/src/simple_commands.py
@@ -5,14 +5,13 @@ import math
 import time
 from sensor_msgs.msg import LaserScan
 import Move
-#from nav_msgs.msg import Odometry
+
 dist = 3
 angle = 0
 speed = 3
 rot = false
 
 #dist and angle would be replaced by the properties of message
-#I just use 'wheel_control()' here, but it'll likely be changed
 pub = rospy.Publisher('/move_simple', Move, queue_size=100)
 def easy (dist,angle,speed,rot):
 	if(angle is 0 and speed not 0 and dist not 0):
@@ -37,14 +36,6 @@ def straight_line(dist,speed):
 		time.sleep(dist/speed) #'sleeps' for the amount of time it takes to travel given distance, then sets all speeds to 0
 		#This can be replaced/enhanced with odometry, since it's right now not very accurate.
 		pub.publish(0,0,0)
-
-
-
-
-
-
-
-
 
 #def listener():
     #rospy.init_node('listener', anonymous=True)
