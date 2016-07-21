3a475459508aacfbb3f5ab4463eb8f17977b31d9
anishau <anisha@uppugonduri.net>
2016-07-20 17:30:56 -0700

    Update quicktest.py

--- basic_move/src/quicktest.py
+++ basic_move/src/quicktest.py
@@ -4,17 +4,17 @@
   from std_msgs.msg import Float32
   
   
-   def talker():
-    pub = rospy.Publisher('testp', float32 , queue_size=10)
-    rospy.init_node('quicktest', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
+  def talker():
+   pub = rospy.Publisher('testp', float32 , queue_size=10)
+   rospy.init_node('quicktest', anonymous=True)
+   rate = rospy.Rate(10) # 10hz
+   while not rospy.is_shutdown():
         
-        pub.publish(float(raw_input("yaw")) )
-        rate.sleep()
+    pub.publish(float(raw_input("yaw")) )
+    rate.sleep()
 
-if __name__ == '__main__':
-    try:
-        talker()
+  if __name__ == '__main__':
+   try:
+    talker()
     except rospy.ROSInterruptException:
-        pass
+     pass
