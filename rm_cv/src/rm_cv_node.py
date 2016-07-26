9fcc33920340dd5b3e7399cbfb7b5fa1ab507e13
pico <pico737@gmail.com>
2016-07-25 21:22:51 -0700

    pipe from yolo using pexpect instead, add client

--- rm_cv/src/rm_cv_node.py
+++ rm_cv/src/rm_cv_node.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 import time
-import math
-import threading
-import subprocess
+import pexpect
+import sys
 
 # ros imports
 import rospy
@@ -21,29 +20,38 @@ class RMCV:
         rospy.init_node('rm_cv', anonymous=True)
 
         # call the cv program
-        cv_process = subprocess.Popen(["rm_darknet_yolo"], shell=True, stdout=subprocess.PIPE)
+        cv_process = pexpect.spawn("/home/pico/catkin_ws/src/robomasters_ros/rm_cv/src/client.py")
 
-        #while not rospy.is_shutdown():
-        while cv_process.poll() is None:
-            line = cv_process.stdout.readline()
-            if line == "---":
-                color = cv_process.stdout.readline()
-                distance = float(cv_process.stdout.readline())
-                y_rotation = float(cv_process.stdout.readline())
-                z_rotation = float(cv_process.stdout.readline())
-                if (color == enemy_color): # only shoot at enemy robots!
-                    # publish the detected robot
-                    dr_req = DetectedRobot()
-                    dr_req.distance = distance
-                    dr_req.y_rotation = self.dr_y_rotation
-                    dr_req.z_rotation = self.dr_z_rotation
-                    self.dr_pub.publish(dr_req)
-
+        while not rospy.is_shutdown():
+            try:
+                cv_process.expect('\n')
+                line = cv_process.before.rstrip()
+                if line == "---":
+                    cv_process.expect('\n')
+                    color = cv_process.before.rstrip()
+                    cv_process.expect('\n')
+                    distance = float(cv_process.before.rstrip())
+                    cv_process.expect('\n')
+                    y_rotation = float(cv_process.before.rstrip())
+                    cv_process.expect('\n')
+                    z_rotation = float(cv_process.before.rstrip())
+                    if (color == enemy_color): # only shoot at enemy robots!
+                        # publish the detected robot
+                        dr_req = DetectedRobot()
+                        dr_req.distance = distance
+                        dr_req.y_rotation = y_rotation
+                        dr_req.z_rotation = z_rotation
+                        self.dr_pub.publish(dr_req)
+            except pexpect.EOF:
+                break
         print "ros shutdown"
         rospy.signal_shutdown("user shutdown command")
 
 if __name__ == '__main__':
     try:
-        RMCV('red')
+        if (len(sys.argv) == 2 and (sys.argv[1] == "red" or sys.argv[1] == "blue")):
+            RMCV(sys.argv[1])
+        else:
+            print "usage: rm_cv_node.py <enemy_color>"
     except rospy.ROSInterruptException:
         pass
