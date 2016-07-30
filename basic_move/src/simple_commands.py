e4b42cde88adc78d8eac324001d91e27fa8c722e
anishau <anisha@uppugonduri.net>
2016-07-30 16:48:54 -0700

    implemented random walk *untested*

--- basic_move/src/simple_commands.py
+++ basic_move/src/simple_commands.py
@@ -8,13 +8,20 @@ from follow_self import*
 from basic_move.msg import Move
 
 
+
 def listener():
     rospy.init_node('simple_commands', anonymous=True)
-    rospy.Subscriber("/chassis_tomove", Move, easy)
+    rospy.Subscriber("/chassis_tomove", Move, tomove_handle)
+    rospy.Subscriber('/scan',LaserScan,handle_scan)
     rospy.spin()
 
+#TODO: fix the time.sleeps
 #dist and angle would be replaced by the properties of message
 pub = rospy.Publisher('/trapezoid/setpoint_chassis', Point, queue_size=100)
+
+def tomove_handle(data):
+	easy(data,null)
+
 def easy (data):
 	if(data.angle is 0 and data.speed !=0 and data.dist!= 0):
 		straight_line(data.dist,data.speed)
@@ -29,15 +36,62 @@ def easy (data):
 			pub.publish(0,data.speed,0)
 			time.sleep(x/data.speed)
 			straight_line(data.dist,data.speed)
-	else:
-		pub.publish(0,0,0)
+
 			
+
+def handle_scan(data):
+	farthest = move_pos(data)
+ 	angle = data.angle_increment*(farthest(2)[bot-1]+farthest(2)[0])/2
+ 	pub.publish(50,0,angle)
+
+
+
 
 def straight_line(dist,speed):
 		pub.publish(speed,0,0) 
 		time.sleep(dist/speed) #'sleeps' for the amount of time it takes to travel given distance, then sets all speeds to 0
 		#This can be replaced/enhanced with odometry, since it's right now not very accurate.
-		pub.publish(0,0,0)
+		pub.publish(0,0,0), 
+
+def move_pos(scan,angle_increment):
+
+    time = 0
+    max = 0
+    pos = 0
+    current_angle = 0
+    currentLength = 0
+    checkerold = []
+    checkernew = [] #temp
+    indiceold = []
+    indicenew = []#temp
+    for i, item in enumerate(scan.ranges):
+    	stable = scan.ranges[0]
+    	time+=1
+    	current_angle = (scan.angle_increment*time)
+        if scan.ranges[i] >= (stable)*.5:
+            if(currentLength==0):
+                checkernew = []
+                indicenew = []
+            currentLength+=1
+            if currentLength > max:
+                max = currentLength
+            checkernew.append(item)
+            indicenew.append(i)
+        else:
+            if(max==currentLength):
+                checkerold = checkernew
+                indiceold = indicenew
+            currentLength = 0
+            stable = scan.ranges[i]
+    pos = 0
+    shape = len(indiceold)
+    if(shape > 0):
+	    if(shape %2 ==0):
+	    	pos = ((checkerold[shape/2]) + (checkerold[shape/2 - 1]))/2
+	    else:
+	    	pos = (checkerold[(shape-1)/2])	 
+    current_angle = len(indiceold)/2 * scan.angle_increment
+    return (checkerold,indiceold)
 
 #def listener():
     #rospy.init_node('listener', anonymous=True)
